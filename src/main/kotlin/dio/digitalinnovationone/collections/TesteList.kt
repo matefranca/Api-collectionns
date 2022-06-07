0dc8cec7da1b68b8aad3117b5173d2c2da6ee75f
Matheus Waked Franca <matheus363@hotmail.com>
2022-06-07 17:56:20 -0300

    Group by

--- src/main/kotlin/dio/digitalinnovationone/collections/TesteList.kt
+++ src/main/kotlin/dio/digitalinnovationone/collections/TesteList.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val joao = Funcionario("Joao", 2000.0)
-    val pedro = Funcionario("Pedro", 1500.0)
-    val maria = Funcionario("Maria", 4000.0)
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
 
@@ -16,11 +16,19 @@ fun main() {
     funcionarios
         .sortedBy { it.salario }
         .forEach {println(it)}
+
+    println("-----------------")
+
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach {println(it)}
+
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ){
     override fun toString(): String =
         """
